fb1cebe28466e97c8643dd8296e23e59924b9b5e
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-07-24 07:16:06 +0300

    home page nav bar added

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import Image from "next/image"
+import SearchBar from "./SearchBar"
+import { Bell, Home, ShoppingCart } from "lucide-react"
+
+const NavBar = () => {
+    return(
+        <nav className = "flex items-center justify-between pb-4 border-b border-gray-200">
+            <Link href = "/" className ="flex items-center gap-2  ">
+                <Image src = "/logo.png" alt = "Logo" width = {36} height = {36}  className = "w-6 h-6 md:w-9  md:h-9"/>
+                <p className = "hidden md:block font-medium text-md tracking-wider">TRENDLAMA</p>
+            </Link>
+            <div className = "flex items-center gap-6">
+                <SearchBar/>
+                <Link href="/">
+                    <Home className = "w-4 h-4 text-gray-500"/>
+                </Link>
+                <Bell className = "w-4 h-4 text-gray-500"/>
+                <ShoppingCart className = "w-4 h-4 text-gray-500"/>
+                <Link href = "/login">sign In</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import { Search } from "lucide-react";
+
+const SearchBar = () => {
+    return(
+        <div className = "hidder sm:flex items-center gap-2 border border-gray-200 rounded-md px-2 py-1">
+            <Search className="text-gray-500 w-4 h-4" />
+            <input type="text" placeholder = "Search..." className = "text-sm outline-0"/>
+        </div>
+    )
+}
+
+export default SearchBar;
